a25b50a4675cfd0062688f14af79038716f677f7
Krishna <Krishna@teamcitydemo>
2017-08-21 17:42:05 +0000

    Modified

---
+++ TeamCityHPFortifyPOC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TeamCityHPFortifyPOC.Startup))]
+namespace TeamCityHPFortifyPOC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
